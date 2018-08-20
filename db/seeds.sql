ee7790af8af6057995ab0dbf04a052d61be3b5e7
homsau <james.p.cowart@gmail.com>
2018-08-20 14:32:35 -0400

    final modifications. works. need to add reame and css tweaks

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name, devoured) VALUES
+("Olive and Let Die Burger", false),
+("Thank God It's Fried Eggs Burger", false),
+("New Bacon-ings Burger", false),
+("Mission A-Corn-Plished Burger", false);
+
+SELECT * FROM burgers
